6c93eb2799e1650fe0f5b5069ec000af35e8f3ba
eric-jy-park <2019147551@yonsei.ac.kr>
2025-07-03 17:33:31 +0900

    fix: fix migration bug

--- apps/server/src/migrations/1751387105551-AddUserTimestamps.ts
+++ apps/server/src/migrations/1751387105551-AddUserTimestamps.ts
@@ -3,9 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AddUserTimestamps1751387105551 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "user" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP`);
-    await queryRunner.query(
-      `ALTER TABLE "user" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP`,
-    );
+    await queryRunner.query(`ALTER TABLE "user" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
