8b28d9f4aaa82e97b2bbcd69c65f8ab8f043893f
eric-jy-park <2019147551@yonsei.ac.kr>
2025-07-03 17:12:29 +0900

    feat: add new migration file

--- apps/server/src/migrations/1751387105551-AddUserTimestamps.ts
+++ apps/server/src/migrations/1751387105551-AddUserTimestamps.ts
@@ -1,11 +1,13 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddUserUpdatedAt1751387105551 implements MigrationInterface {
+export class AddUserTimestamps1751387105551 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP`);
     await queryRunner.query(`ALTER TABLE "user" ADD "updatedAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "createdAt"`);
     await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "updatedAt"`);
   }
 }
